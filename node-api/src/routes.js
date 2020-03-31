b3d83dcdf89035ecc9f1d2dac141c987af23edef
BoltzBit <mateus_desktop@outlook.com>
2020-03-31 12:03:44 -0300

    aulas

---
+++ node-api/src/routes.js
@@ -0,0 +1,12 @@
+//deixar as rotas separadas
+const express = require('express');
+const routes = express.Router();
+const ProductController = require('./controllers/ProductController');
+//metodo para pegar requisicao e respostas
+routes.get('/products', ProductController.index);
+routes.get('/products/:id',ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id',ProductController.destroy);
+
+module.exports = routes;
